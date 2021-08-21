864d6a1072365d7c67e33ba5a851e8d3835b02b6
rider <myintthwaykhine127@gmail.com>
2021-08-21 06:56:37 +0630

    partials and components folders commit

--- views/about.view.php
+++ views/about.view.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php require "views/partials/header.php"; ?>
     <h1>This is About</h1>
-</body>
-</html>
+<?php require "views/partials/footer.php"; ?>
--- views/index.view.php
+++ views/index.view.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php require "views/partials/header.php"; ?>
     <h1>My Job</h1>
     <?php foreach($tasks as $task): ?>
         <?php
@@ -20,5 +12,4 @@
             }
         ?>
     <?php endforeach; ?>
-</body>
-</html>
+<?php require "views/partials/footer.php"; ?>
