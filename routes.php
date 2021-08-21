9b70f052b2459bac14943b2d4c0ccc320d8eec77
rider <myintthwaykhine127@gmail.com>
2021-08-21 07:00:56 +0630

    Eg.Order router adding commit

--- routes.php
+++ routes.php
@@ -2,5 +2,6 @@
     $router->register([
         ""=>"controllers/indexController.php",
         "about"=>"controllers/aboutController.php",
+        "order"=>"controllers/orderController.php",
     ]);
 ?>
---
+++ views/order.view.php
@@ -0,0 +1,3 @@
+<?php require "views/partials/header.php"; ?>
+    <h1>This is Order</h1>
+<?php require "views/partials/footer.php"; ?>
